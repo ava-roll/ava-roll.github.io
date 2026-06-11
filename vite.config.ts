32cc66a0b35f335b7509d34b7e919760fb8da960
zizlak <ak.moabit@gmail.com>
2026-06-11 07:07:31 +0200

    dice and run

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/gif-board-bonanza/",
+  base: "/roll-and-race/",
   server: {
     host: "::",
     port: 8080,
